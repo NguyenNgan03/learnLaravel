bd9fc8c8a1d123f163e40895a7be5264c5df16ac
NguyenNgan03 <simple.270123@gmail.com>
2024-02-20 10:56:02 +0700

    bài 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1> home laravel </h1>
-</body>
-</html>
+<h1> hello</h1>
+<?php
+if (env('APP_ENV') == 'production') {
+    echo 'call api live';
+} else {
+    echo 'call api sandbox';
+}
+?>
